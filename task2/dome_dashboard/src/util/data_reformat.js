523a2a22a9aa0311478733d32049c4836554c5ba
Sothis Baka <archmagus02@gmail.com>
2023-04-27 17:02:06 -0700

    cyl-task2-dev divide the data by the id

--- task2/dome_dashboard/src/util/data_reformat.js
+++ task2/dome_dashboard/src/util/data_reformat.js
@@ -9,6 +9,7 @@ import {
     Legend,
     Colors,
 } from 'chart.js';
+import {group} from 'plotly.js/src/plots/frame_attributes';
 
 ChartJS.register(
     CategoryScale,
@@ -25,11 +26,22 @@ const line_chart_reformat_data = (json_data, x_param, y_param_list) => {
     const datasets = []
 
     for (let y_param of y_param_list) {
-        datasets.push({
-            label: y_param,
-            data: json_data.map(entry => entry[y_param]),
-            fill: false,
-        })
+        const grouped_data = {}
+
+        for (let entry of json_data) {
+            if (!entry.id) entry.id = "default";
+            const id = entry.id;
+            if (!grouped_data[id]) grouped_data[id] = [];
+            grouped_data[id].push(entry);
+        }
+
+        for (let id in grouped_data) {
+            datasets.push({
+                label: y_param,
+                data: (grouped_data[id]).map(entry => entry[y_param]),
+                fill: false,
+            })
+        }
     }
 
     return {
